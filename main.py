aef642ccf6fa8b3c1e3f826ddecf07cfcde359ed
James <james@Elevate-MacBookPro.local>
2023-07-29 22:25:42 +0200

    Add database reader skeleton func

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import polars as pl
+from sqlalchemy.engine.url import URL
 
 
-def get_cyrpto_market_data(api_url: str, token: str):
+def req_cyrpto_market_data(api_url: str, token: str):
     """Send API request to coinmarketcap to get market data"""
     response = requests.api.get(
         url=api_url,
@@ -11,13 +12,29 @@ def get_cyrpto_market_data(api_url: str, token: str):
         }
     )
 
-    return response.json()
+    return pl.DataFrame(response.json()["data"])
+
+
+def read_time_checkpoint(timeframe: str, db_url: URL):
+    """Read the historic prices
+    
+    param timeframe: str
+    param options: 
+        <2,4>HR, <(1-7)>DAY 
+    """
+    table = ""
+    timeframe_column = ""
+    sql_query = f"SELECT * FROM {table} WHERE {timeframe_column} = {timeframe};"
+
+    return pl.read_database(
+        query=sql_query,
+        connection=db_url
+    )
 
 
 if __name__ == "__main__":
-    resonse_data = get_cyrpto_market_data(
+    response = req_cyrpto_market_data(
         api_url="https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
         token="b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
     )
     breakpoint()
-    crypto_df = pl.DataFrame(resonse_data["data"])
